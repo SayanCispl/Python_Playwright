b0e2b018fb2e628027eb7e2ba09ccf4a965c0cf6
SayanCispl <sayan.koley@codeclouds.co.in>
2026-03-25 17:43:48 +0530

    Fix Github Action

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Settings:
     BASE_URL = os.getenv("BASE_URL")
     SHOP_URL = os.getenv("SHOP_URL", f"{BASE_URL}/products")
-    USERNAME = os.getenv("USERNAME")
+    APP_USERNAME = os.getenv("APP_USERNAME")   # ✅ avoids system USERNAME clash
     PASSWORD = os.getenv("PASSWORD")
     test_name = "checkout_flow"
